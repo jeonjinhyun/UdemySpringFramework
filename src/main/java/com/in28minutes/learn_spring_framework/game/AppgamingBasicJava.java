90d8859ff6aec9cd38eacfc3552d5d86a5e6f9e3
jeonjinhyun <jjh06500@gmail.com>
2024-10-17 08:49:07 +0900

    9

---
+++ src/main/java/com/in28minutes/learn_spring_framework/game/AppgamingBasicJava.java
@@ -0,0 +1,9 @@
+package com.in28minutes.learn_spring_framework.game;
+
+public class AppgamingBasicJava {
+    public static void main(String[] args) {
+        var marioGame = new MarioGame();
+        var gameRunner = new GameRunner(marioGame);
+        gameRunner.run();
+    }
+}
