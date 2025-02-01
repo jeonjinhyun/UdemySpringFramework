5692569892687f0772b8761f12a2613ce900d6f1
jeonjinhyun <jjh06500@gmail.com>
2025-02-01 10:31:23 +0900

    GameRunner

--- src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
+++ src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
@@ -1,17 +1,22 @@
 package com.in28minutes.learn_spring_framework.game;
 
+//PacmanGame
 public class GameRunner {
-    MarioGame game;
-    public GameRunner(MarioGame game) {
-        this.game = game;
-    }
+	
+	private GamingConsole game;
+	
+	public GameRunner(GamingConsole game) {
+		this.game = game;
+	}
 
-    public void run() {
-        System.out.println("Running game: "+game);
-        game.up();
-        game.down();
-        game.left();
-        game.right();
-    }
+	public void run() {
+		
+		System.out.println("Running game: " + game);
+		game.up();
+		game.down();
+		game.left();
+		game.right();
+		
+	}
+
 }
-
