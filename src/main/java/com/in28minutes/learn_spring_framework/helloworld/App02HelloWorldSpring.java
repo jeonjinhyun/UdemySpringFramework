53c18e0629e070e4c32c6844c1494ddec8b4fc6a
jeonjinhyun <jjh06500@gmail.com>
2025-02-04 10:31:23 +0900

    App02HelloWorldSpring

--- src/main/java/com/in28minutes/learn_spring_framework/helloworld/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learn_spring_framework/helloworld/App02HelloWorldSpring.java
@@ -5,20 +5,48 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import java.util.Arrays;
 
-@SpringBootApplication
 public class App02HelloWorldSpring {
-    public static void main(String[] args) {
-        var context = new AnnotationConfigApplicationContext(App02HelloWorldSpring.class);
 
-        System.out.println(context.getBean("name"));
-        System.out.println(context.getBean("age"));
-        System.out.println(context.getBean("person"));
-        System.out.println(context.getBean("person2MethodCall"));
-        System.out.println(context.getBean("person3Parameters"));
-        System.out.println(context.getBean("address2"));
-        System.out.println(context.getBean(Address.class));
+	public static void main(String[] args) {
 
-        Arrays.stream(context.getBeanDefinitionNames())
-                .forEach(System.out::println);
-    }
+		//1: Launch a Spring Context
+		try(var context = 
+				new AnnotationConfigApplicationContext
+						(HelloWorldConfiguration.class)) {
+			
+			//2: Configure the things that we want Spring to manage - 
+			//HelloWorldConfiguration - @Configuration
+			//name - @Bean
+			
+			//3: Retrieving Beans managed by Spring
+			System.out.println(context.getBean("name"));
+			
+			System.out.println(context.getBean("age"));
+			
+			System.out.println(context.getBean("person"));
+			
+			System.out.println(context.getBean("person2MethodCall"));
+			
+			System.out.println(context.getBean("person3Parameters"));
+			
+			System.out.println(context.getBean("address2"));
+			
+			System.out.println(context.getBean(Person.class));
+			
+			System.out.println(context.getBean(Address.class));
+			
+			System.out.println(context.getBean("person5Qualifier"));
+			
+			
+			//System.out.println
+//			Arrays.stream(context.getBeanDefinitionNames())
+//				.forEach(System.out::println);
+			
+		}
+		
+		
+		
+		
+	}
+
 }
