8d5b78e3ce7d212bfcb075d16f30d240ba4e188b
jeonjinhyun <jjh06500@gmail.com>
2025-02-10 10:31:23 +0900

    GamingConfiguration

---
+++ src/main/java/com/in28minutes/learn_spring_framework/GamingConfiguration.java
@@ -0,0 +1,19 @@
+package main.java.com.in28minutes.learn_spring_framework;
+
+
+@Configuration
+public class GamingConfiguration {
+
+	@Bean
+	public GamingConsole game() {
+		var game = new PacmanGame();
+		return game;
+	}
+
+	@Bean
+	public GameRunner gameRunner(GamingConsole game) {
+		var gameRunner = new GameRunner(game);
+		return gameRunner;
+	}
+
+}
