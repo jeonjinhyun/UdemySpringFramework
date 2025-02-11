2bf928a43c17af5c88cd2599db661ed92204df16
jeonjinhyun <jjh06500@gmail.com>
2025-02-11 10:31:23 +0900

    BusinessCalculationService

---
+++ src/main/java/com/in28minutes/spring_01/learn_spring_framework_02/examples/c1/BusinessCalculationService.java
@@ -0,0 +1,17 @@
+package com.in28minutes.spring_01.learn_spring_framework_02.examples.c1;
+//@Component
+@Service
+public class BusinessCalculationService {
+	
+	private DataService dataService;
+	
+	public BusinessCalculationService(DataService dataService) {
+		super();
+		this.dataService = dataService;
+	}
+
+	public int findMax() {
+		return Arrays.stream(dataService.retrieveData()).max().orElse(0);
+	}
+
+}
