eae6c55f33212de0c50acdcd597b35f0adad2093
jeonjinhyun <jjh06500@gmail.com>
2025-02-21 10:31:23 +0900

    XmlConfigurationContextLauncherApplication

---
+++ src/main/java/com/in28minutes/spring_01/learn_spring_framework_02/examples/h1/XmlConfigurationContextLauncherApplication.java
@@ -0,0 +1,20 @@
+package com.in28minutes.spring_01.learn_spring_framework_02.examples.h1;
+
+public class XmlConfigurationContextLauncherApplication {
+	
+	public static void main(String[] args) {
+
+		try (var context = 
+				new ClassPathXmlApplicationContext("contextConfiguration.xml")) {
+			
+			Arrays.stream(context.getBeanDefinitionNames())
+				.forEach(System.out::println);
+			
+			System.out.println(context.getBean("name"));
+			
+			System.out.println(context.getBean("age"));
+			
+			context.getBean(GameRunner.class).run();
+
+		}
+	}
