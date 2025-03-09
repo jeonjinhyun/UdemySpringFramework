df3b3b8f8610413851c527a86a6aa5d0539a5ab0
jeonjinhyun <jjh06500@gmail.com>
2025-03-09 10:31:23 +0900

    CourseJpaRepository

---
+++ src/main/java/com/in28minutes/jpa_getting_started_03/course/jpa/CourseJpaRepository.java
@@ -0,0 +1,23 @@
+package com.in28minutes.jpa_getting_started_03.course.jpa;
+
+@Repository
+@Transactional
+public class CourseJpaRepository {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public void insert(Course course) {
+		entityManager.merge(course);
+	}
+	
+	public Course findById(long id) {
+		return entityManager.find(Course.class, id);
+	}
+
+	public void deleteById(long id) {
+		Course course = entityManager.find(Course.class, id);
+		entityManager.remove(course);
+	}
+
+}
