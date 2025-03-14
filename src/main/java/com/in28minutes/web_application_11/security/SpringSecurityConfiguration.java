9c2dafda206028f9bfac44070d06aa379f4c2d47
jeonjinhyun <jjh06500@gmail.com>
2025-03-14 10:31:23 +0900

    SpringSecurityConfiguration

---
+++ src/main/java/com/in28minutes/web_application_11/security/SpringSecurityConfiguration.java
@@ -0,0 +1,63 @@
+package com.in28minutes.web_application_11.security;
+
+@Configuration
+public class SpringSecurityConfiguration {
+	//LDAP or Database
+	//In Memory 
+	
+	//InMemoryUserDetailsManager
+	//InMemoryUserDetailsManager(UserDetails... users)
+	
+	@Bean
+	public InMemoryUserDetailsManager createUserDetailsManager() {
+		
+		UserDetails userDetails1 = createNewUser("in28minutes", "dummy");
+		UserDetails userDetails2 = createNewUser("ranga", "dummydummy");
+		
+		return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+	}
+
+	private UserDetails createNewUser(String username, String password) {
+		Function<String, String> passwordEncoder
+		= input -> passwordEncoder().encode(input);
+
+		UserDetails userDetails = User.builder()
+									.passwordEncoder(passwordEncoder)
+									.username(username)
+									.password(password)
+									.roles("USER","ADMIN")
+									.build();
+		return userDetails;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	//All URLs are protected
+	//A login form is shown for unauthorized requests
+	//CSRF disable
+	//Frames
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests(
+				auth -> auth.anyRequest().authenticated());
+		http.formLogin(withDefaults());
+		
+		http.csrf(AbstractHttpConfigurer::disable);
+		// OR
+		// http.csrf(AbstractHttpConfigurer::disable);
+
+		http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)); // Starting from SB 3.1.x
+		
+		return http.build();
+	}
+	
+	
+	
+	
+	
+}
