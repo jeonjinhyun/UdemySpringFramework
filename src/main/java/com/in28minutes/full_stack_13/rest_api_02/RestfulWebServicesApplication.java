5550934755be9e68e0a65187b0f574489ca001be
jeonjinhyun <jjh06500@gmail.com>
2025-05-11 10:31:23 +0900

    RestfulWebServicesApplication

---
+++ src/main/java/com/in28minutes/full_stack_13/rest_api_02/RestfulWebServicesApplication.java
@@ -0,0 +1,23 @@
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+	
+	//http://localhost:3000/ to 8080
+	//Cross Origin Requests
+	//Allow all requests only from http://localhost:3000/
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedMethods("*")
+					.allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
+
+}
