2eaa5011aa76bbfd250ec897d66ae07874ca9d3c
jeonjinhyun <jjh06500@gmail.com>
2025-05-14 10:31:23 +0900

    HelloWorldBean

---
+++ src/main/java/com/in28minutes/full_stack_13/rest_api_02/helloworld/HelloWorldBean.java
@@ -0,0 +1,23 @@
+package com.in28minutes.full_stack_13.rest_api_02.helloworld;
+
+public class HelloWorldBean {
+
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+}
