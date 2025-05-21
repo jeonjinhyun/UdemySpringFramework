3da93201f74c11cb2e93fed67c1c6f248e6eb583
jeonjinhyun <jjh06500@gmail.com>
2025-05-21 10:31:23 +0900

    d

--- src/main/java/com/in28minutes/spring_security_71/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/in28minutes/spring_security_71/basic/BasicAuthSecurityConfiguration.java
@@ -84,6 +84,6 @@ public class BasicAuthSecurityConfiguration {
 	
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder()
+        return new BCryptPasswordEncoder();
     }
 }
