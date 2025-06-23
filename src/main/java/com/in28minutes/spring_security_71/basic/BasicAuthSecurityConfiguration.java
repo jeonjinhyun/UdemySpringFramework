c5984df611142b42cb319c456ee8a12077b09990
jeonjinhyun <jjh06500@gmail.com>
2025-06-23 11:16:40 +0900

    BasicAuthSecurityConfiguration

---
+++ src/main/java/com/in28minutes/spring_security_71/basic/BasicAuthSecurityConfiguration.java
@@ -0,0 +1,87 @@
+package com.in28minutes.spring_security_71.basic;
+
+@Configuration
+@EnableMethodSecurity(jsr250Enabled = true, securedEnabled = true)
+public class BasicAuthSecurityConfiguration {
+	
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
+		http.authorizeHttpRequests(
+						auth -> {
+							auth
+							.anyRequest().authenticated();
+						});
+		
+		http.sessionManagement(
+						session -> 
+							session.sessionCreationPolicy(
+									SessionCreationPolicy.STATELESS)
+						);
+		
+		//http.formLogin();
+		http.httpBasic(withDefaults());
+
+		http.csrf(csrf -> csrf.disable());
+
+		//http.csrf(AbstractHttpConfigurer::disable);
+
+		http.headers(headers -> headers.frameOptions(frameOptionsConfig-> frameOptionsConfig.disable()));
+
+		// http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+		
+		return http.build();
+	}
+
+//	@Bean
+//	public UserDetailsService userDetailService() {
+//		
+//		var user = User.withUsername("in28minutes")
+//			.password("{noop}dummy")
+//			.roles("USER")
+//			.build();
+//
+//		
+//		var admin = User.withUsername("admin")
+//				.password("{noop}dummy")
+//				.roles("ADMIN")
+//				.build();
+//
+//		return new InMemoryUserDetailsManager(user, admin);
+//	}
+	
+	@Bean
+	public DataSource dataSource() {
+		return new EmbeddedDatabaseBuilder()
+				.setType(EmbeddedDatabaseType.H2)
+				.addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+				.build();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailService(DataSource dataSource) {
+		
+		var user = User.withUsername("in28minutes")
+			//.password("{noop}dummy")
+			.password("dummy")
+			.passwordEncoder(str -> passwordEncoder().encode(str))
+			.roles("USER")
+			.build();
+		
+		var admin = User.withUsername("admin")
+				//.password("{noop}dummy")
+				.password("dummy")
+				.passwordEncoder(str -> passwordEncoder().encode(str))
+				.roles("ADMIN", "USER")
+				.build();
+		
+		var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+		jdbcUserDetailsManager.createUser(user);
+		jdbcUserDetailsManager.createUser(admin);
+
+		return jdbcUserDetailsManager;
+	}
+	
+    @
+	
+}
